046356f257d31156d4ea0d0d0cb9bd72e44cb87d
Víctor <victorlopez94leon@gmail.com>
2016-05-27 13:16:18 +0200

    Añadida la subclase de EmbarcacionDeportiva Yate

---
+++ Yate.java
@@ -0,0 +1,29 @@
+public class Yate extends EmbarcacionDeportiva
+{
+    private int camarotes;
+
+    /**
+     * Constructor de objetos de la clase Yate
+     */
+    public Yate(int camarotes,int potencia, String matricula, float eslora, int anoFabricacion)
+    {
+        super(potencia,matricula,eslora,anoFabricacion);
+        this.camarotes = camarotes;
+    }
+
+    /**
+     * Devuelve el coeficiente de Bernua del yate
+     */
+    @Override
+    public int getCoeficienteBernua() {
+        return getCoeficienteBernua() + camarotes;
+    }
+    
+    /**
+     * Devuelve un String ocn la información del objeto
+     */
+    @Override
+    public String toString() {
+        return toString() + " Camarotes:" + camarotes;
+    }
+}
