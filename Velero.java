4a837a9de9caed16d0d4a1e92059696d433cd709
Víctor <victorlopez94leon@gmail.com>
2016-05-27 12:59:50 +0200

    Añadida la subclase de Barco Velero

---
+++ Velero.java
@@ -0,0 +1,29 @@
+public class Velero extends Barco
+{
+    private int numeroMastiles;
+
+    /**
+     * Constructor de objetos de la clase Velero
+     */
+    public Velero(int numeroMastiles, String matricula, float eslora, int anoFabricacion)
+    {
+        super(matricula,eslora,anoFabricacion);
+        this.numeroMastiles = numeroMastiles;
+    }
+
+    /**
+     * Devuelve el coeficiente de Bernua del Velero
+     */
+    @Override
+    public int getCoeficienteBernua() {
+        return numeroMastiles;
+    }
+    
+    /**
+     * Devuelve un String con la información
+     */
+    @Override
+    public String toString() {
+        return toString() + " Nº de mastiles: " + numeroMastiles;
+    }
+}
